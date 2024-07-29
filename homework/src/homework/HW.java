2f8530747739b8662a92577daa7d6f8fbff4c35b
Shitolee <111382237+Shitolee@users.noreply.github.com>
2024-07-29 13:22:13 +0530

    Initial commit

---
+++ homework/src/homework/HW.java
@@ -0,0 +1,34 @@
+package homework;
+
+public class HW {
+	
+		
+	    static boolean bool;
+	    static byte b;
+		static short s;
+		static int i;
+		static long l;
+		static float f;
+		static double d;
+		static char c;
+		static String S;
+			
+		
+		
+		public static void main(String[] args) {
+			HW h = new HW();
+			System.out.println("Default values of datatypes are :");
+			System.out.println("boolean :" + bool);
+			System.out.println("Byte :" + b);
+			System.out.println("Short :" + s);
+			System.out.println("Int :" + i);
+			System.out.println("long :" + l);
+			System.out.println("float :" + f);
+			System.out.println("Double :" + d);
+			System.out.println("char :" + c);
+			System.out.println("String :" + S);
+		}
+		
+	}
+
+
